a2e4eb16ee25e1d69adf60e25a16e9bc6a6b3ce1
Damian Jarzebowski <damian.r.hajduk@o2.pl>
2022-11-24 00:51:25 +0100

    delete entity what wil be useless

--- src/main/java/dj/repository/CompaniesRepository.java
+++
@@ -1,9 +0,0 @@
-package dj.repository;
-
-import dj.models.identity.other.Companies;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CompaniesRepository extends JpaRepository<Companies, Long> {
-}
--- src/main/java/dj/repository/ProjectRepository.java
+++
@@ -1,7 +0,0 @@
-package dj.repository;
-
-import dj.models.identity.other.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-}
